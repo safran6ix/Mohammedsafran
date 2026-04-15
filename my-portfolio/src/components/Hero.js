cc8649bd001066c08948a2b2eee9e52b92c0b075
Safran6ix <mohammedsafran6ix@gmail.com>
2026-04-15 15:53:26 +0530

    Adding Hero.js

---
+++ my-portfolio/src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Hero() {
+    return (
+        <section className="hero">
+            <h1>Hi, I'm Safran</h1>
+            <p>Frontend Developer | React Enthusiast</p>
+            <button>View My Work</button>
+        </section>
+    );
+}
+
+export default Hero;
