dd2338537a7ed4dd8f9f80b6972005202ecc8fc5
Safran6ix <mohammedsafran6ix@gmail.com>
2026-04-15 14:41:34 +0530

    update

--- my-portfolio/src/components/Header.js
+++ my-portfolio/src/components/Header.js
@@ -1,1 +1,19 @@
-helloo
+import React from "react";
+
+function Header() {
+    return (
+      <header>
+           <nav>
+              <h2>Portfolio</h2>
+              <ul>
+                <li><a href="#about">About</a></li>
+                <li><a href="#project">Project</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#contact">Contact</a></li>
+              </ul>
+           </nav>
+      </header>
+    );
+}
+
+export default Header;
